799cdb2cdd55cab82ce2ccd82a9b3a3cf586f9f1
gigakhrz <kharazishvilig13@gmail.com>
2023-12-05 15:52:29 +0400

    add edit text

--- components/Todos.tsx
+++ components/Todos.tsx
@@ -4,15 +4,22 @@ import BouncyCheckbox from 'react-native-bouncy-checkbox';
 const Todos = (): JSX.Element => {
   return (
     <View style={style.todosWrapper}>
+      {/*  */}
       <View style={style.taskContainer}>
-        <BouncyCheckbox
-          onPress={(isChecked: boolean) => {
-            isChecked = true;
-          }}
-          fillColor="green"
-          size={27}
-        />
-        <Text style={style.todoTitle}>workout</Text>
+        <View style={style.firstHalf}>
+          <BouncyCheckbox
+            onPress={(isChecked: boolean) => {
+              isChecked = true;
+            }}
+            fillColor="green"
+            size={27}
+          />
+          <Text style={style.todoTitle}>workout</Text>
+        </View>
+
+        <View style={style.changeCont}>
+          <Text style={style.edit}>edit</Text>
+        </View>
       </View>
     </View>
   );
@@ -25,17 +32,32 @@ const style = StyleSheet.create({
     width: '100%',
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'center',
   },
 
   taskContainer: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  firstHalf: {
+    display: 'flex',
+    flexDirection: 'row',
   },
 
   todoTitle: {
-    fontSize: 22,
+    fontSize: 20,
     color: 'black',
+  },
+
+  changeCont: {
+    display: 'flex',
+    gap: 10,
+  },
+
+  edit: {
+    fontSize: 20,
+    color: 'green',
   },
 });
