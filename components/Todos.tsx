bdc0bc6bd6e17c67a3d27a9399bbf663a5c40f4a
gigakhrz <kharazishvilig13@gmail.com>
2023-12-05 15:26:46 +0400

    create todos file

---
+++ components/Todos.tsx
@@ -0,0 +1,9 @@
+import {StyleSheet, View} from 'react-native';
+
+const Todos = (): JSX.Element => {
+  return <View style={style.todosWrapper}></View>;
+};
+
+const style = StyleSheet.create({
+  todosWrapper: {},
+});
