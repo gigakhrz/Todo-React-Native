b580628398fa75dc7c6129de3b7415edfd102e12
gigakhrz <kharazishvilig13@gmail.com>
2023-12-05 15:44:55 +0400

    add totoTitle

--- components/Todos.tsx
+++ components/Todos.tsx
@@ -1,16 +1,18 @@
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
 
 const Todos = (): JSX.Element => {
   return (
     <View style={style.todosWrapper}>
-      <View>
+      <View style={style.taskContainer}>
         <BouncyCheckbox
           onPress={(isChecked: boolean) => {
             isChecked = true;
           }}
           fillColor="green"
+          size={27}
         />
+        <Text style={style.todoTitle}>workout</Text>
       </View>
     </View>
   );
@@ -21,5 +23,19 @@ export default Todos;
 const style = StyleSheet.create({
   todosWrapper: {
     width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+
+  taskContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  todoTitle: {
+    fontSize: 22,
+    color: 'black',
   },
 });
