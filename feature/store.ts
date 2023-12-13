5ed83f9f29d7281d3545102262eeed66584c8cad
gigakhrz <kharazishvilig13@gmail.com>
2023-12-13 15:44:12 +0400

    update

--- feature/store.ts
+++ feature/store.ts
@@ -1,15 +1,18 @@
 import {configureStore} from '@reduxjs/toolkit';
 import todoName, {InputProps} from './todoName';
 import replacebleInput, {EditProps} from './replacebleInput';
+import editingText, {EditingProps} from './editingText';
 
 const store = configureStore({
   reducer: {
     todoName: todoName,
     replacebleInput: replacebleInput,
+    editingText: editingText,
   },
 });
 
 export type RootState = {
   todoName: InputProps;
   replacebleInput: EditProps;
+  editingText: EditingProps;
 };
