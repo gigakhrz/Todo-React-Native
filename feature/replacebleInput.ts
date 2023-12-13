1640e4c2149dabca8d38bae370e07bfc8aaccd14
gigakhrz <kharazishvilig13@gmail.com>
2023-12-13 15:36:55 +0400

    add replacebleInput state

--- feature/replacebleInput.ts
+++ feature/replacebleInput.ts
@@ -1,3 +1,29 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
-interface Replace {}
+interface ReplaceProps {
+  _id: string;
+  title: string;
+  complated: boolean;
+}
+
+export interface EditProps {
+  edit: ReplaceProps[];
+}
+
+const initialState: EditProps = {
+  edit: [],
+};
+
+const replacebleInput = createSlice({
+  name: 'replacebleInput',
+  initialState,
+  reducers: {
+    setEdit: (state, action: PayloadAction<ReplaceProps[]>) => {
+      state.edit = action.payload;
+    },
+  },
+});
+
+export const {setEdit} = replacebleInput.actions;
+
+export default replacebleInput.reducer;
--- feature/store.ts
+++ feature/store.ts
@@ -1,12 +1,15 @@
 import {configureStore} from '@reduxjs/toolkit';
 import todoName, {InputProps} from './todoName';
+import replacebleInput, {EditProps} from './replacebleInput';
 
 const store = configureStore({
   reducer: {
     todoName: todoName,
+    replacebleInput: replacebleInput,
   },
 });
 
 export type RootState = {
   todoName: InputProps;
+  replacebleInput: EditProps;
 };
